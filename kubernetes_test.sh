2091419bc66af4722404a205e71d3bcec15965b7
Chintan <cvora22@gmail.com>
2019-06-16 13:44:37 -0700

    edited location for build script

--- kubernetes_test.sh
+++ kubernetes_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 eval $(minikube docker-env)
-kubectl --kubeconfig ~/.kube/config --server https://192.168.99.105:8443 get nodes
+kubectl --kubeconfig /home/apnatimeayega/.kube/config --server https://192.168.99.105:8443 get nodes
 
