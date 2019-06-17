d040829778dfe270db6974e4d1082b93f67bad50
Chintan <cvora22@gmail.com>
2019-06-17 11:07:12 -0700

    Added fixes to make sure the pipeline is working correctly

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
+echo Printing Skaffold Version
+skaffold version
 
-echo Printing Maven Version
-mvn -version
-
-#eval $(minikube docker-env)
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://192.168.99.105:2376"
 export DOCKER_CERT_PATH="/home/apnatimeayega/.minikube/certs"
 export DOCKER_API_VERSION="1.35"
-mvn compile jib:dockerBuild
 
+echo Running Skaffold to deploy into Minikube environment
+skaffold run
--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo Printing Skaffold Version
-skaffold version
-
-
-echo Running Skaffold to deploy into Minikube environment
-skaffold run
