1e729c9fdad2f81b44286928db639b63013a3403
Chintan <cvora22@gmail.com>
2019-06-23 07:36:28 -0700

    added new message for service

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo Printing Skaffold Version
 skaffold version
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="tcp://192.168.99.105:2376"
+export DOCKER_HOST="tcp://192.168.99.106:2376"
 export DOCKER_CERT_PATH="/home/apnatimeayega/.minikube/certs"
 export DOCKER_API_VERSION="1.35"
 
